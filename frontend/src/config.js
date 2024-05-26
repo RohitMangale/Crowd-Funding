7f883232ba71f7126af917e104244c875074fe80
rohit <119059546+RohitMangale@users.noreply.github.com>
2024-05-26 23:25:19 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://crowd-funding-snowy.vercel.app";
+export const BASE_URL = "https://crowd-funding-snowy.vercel.app/api";
 export const token = localStorage.getItem('token');
