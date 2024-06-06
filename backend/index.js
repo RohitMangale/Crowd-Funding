beb456898bcecd9cdaf3207eddac03385e5d7754
RohitMangale <119059546+RohitMangale@users.noreply.github.com>
2024-06-06 19:59:15 +0530

    connection.1

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,9 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { authRoute} from "./Routes/auth.js";
+
+// Routes
+import { authRoute } from "./Routes/auth.js";
 import { investorRoute } from "./Routes/investor.js";
 import { FundraiserRoute } from "./Routes/fundraiser.js";
 import { InternRoute } from "./Routes/intern.js";
@@ -22,19 +24,6 @@ const corsOptions = {
   credentials:true
 };
 
-// mongoose.set("strictQuery", false);
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URL, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log("MongoDB database is connected");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
@@ -45,29 +34,18 @@ app.use("/api/interns", InternRoute);
 app.use("/api/campaigns", CampaignRoute);
 app.use("/api/internships", internshipRoute);
 
-// mongoose
-//   .connect(process.env.MONGO_URL)
-//   .then(() => {
-//     app.listen(port, () => {
-//       console.log("Listening on port " + port);
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+// Error handling middleware (optional, example)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 
 mongoose
   .connect(process.env.MONGO_URL) // specify db name as object or else it will save data in test db
   .then(() => {
     console.log("Connected to Mongoose");
-    console.log("Server Started...");
+    app.listen(port, () => {
+      console.log(`Server Started on port ${port}`);
+    });
   })
   .catch((err) => console.log(err));
-
-app.listen(https://crowd-funding-snowy.vercel.app, () => {
-});
-
-// app.listen(port, () => {
-//   // connectDB();
-//   console.log("Server in runnning on port " + port);
-// });
